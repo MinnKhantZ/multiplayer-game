87bea4a60235f8668aa7f09c462ca8a9cc7a8b6b
MinnKhantZ <minkhankzaw2543@gmail.com>
2024-10-30 19:01:10 +0630

    Initial commit

--- server.js
+++ server.js
@@ -7,8 +7,13 @@ const cors = require('cors');
 
 const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner.js');
+const { default: Player } = require('./public/Player.mjs');
+const { default: Collectible } = require('./public/Collectible.mjs');
 
 const app = express();
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/assets', express.static(process.cwd() + '/assets'));
@@ -35,10 +40,47 @@ app.use(function(req, res, next) {
     .send('Not Found');
 });
 
+const players = {};
+const collectibles = [new Collectible('c1', 100, 150, 10)];
+function calculateRanks() {
+  const sortedPlayers = Object.values(players).sort((a, b) => b.score - a.score);
+  sortedPlayers.forEach((player, index) => {
+    player.rank = `Rank: ${index + 1}/${sortedPlayers.length}`;
+  });
+}
+
+
+io.on('connection', (socket) => {
+  const player = new Player(socket.id, 50, 50);
+  players[socket.id] = player;
+  
+  socket.emit('init', { playerId: socket.id, players, collectibles });
+  
+  socket.broadcast.emit('playerJoined', player);
+  
+  socket.on('move', (directions) => {
+    directions.forEach(direction => player.movePlayer(direction, 5));
+    collectibles.forEach((item, index) => {
+      if (player.collision(item)) {
+        player.score += item.value;
+        collectibles.splice(index, 1);
+        collectibles.push(new Collectible(`c${Date.now()}`, Math.random() * 500, Math.random() * 500, 10));
+      }
+    });
+    calculateRanks();
+    io.emit('gameState', { players, collectibles });
+  });
+  
+  socket.on('disconnect', () => {
+    delete players[socket.id];
+    socket.broadcast.emit('playerDisconnected', socket.id);
+  });
+});
+
 const portNum = process.env.PORT || 3000;
 
 // Set up server and tests
-const server = app.listen(portNum, () => {
+const server = http.listen(portNum, () => {
   console.log(`Listening on port ${portNum}`);
   if (process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
